456e6893ed77ee952faeed9d543e06c5243b6ff6
renaldo001 <45104038+renaldo001@users.noreply.github.com>
2020-04-22 12:08:39 +0700

    Update Test_1.js

--- Test_1.js
+++ Test_1.js
@@ -1,15 +1,15 @@
 function solution(record) {
   var answer = [];
-  for(i = 0 ; i < record.length() ; i ++ ){
+  for(i = 0 ; i < record.length ; i ++ ){
     const [status, userid, nickname] = record[i].split(' ');
     if(status === 'Enter'){
-      answer.push[`${nickname} came in`]
+      answer.push(`${nickname} came in`)
     }
     else if (status === 'Leave'){
-      answer.push[`${nickname} has leave`]
+      answer.push(`${nickname} has leave`)
     }
-    else {
-      for ( j = 0 ; j < answer.length() ; j++){
+    else{
+      for ( j = 0 ; j < answer.length ; j++){
         if ( answer[j].userid === userid ){
           answer[j].nickname === nickname
         }
