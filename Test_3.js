906fe561cb4946d8d382bbb36820d88fd14113a0
renaldo001 <45104038+renaldo001@users.noreply.github.com>
2020-04-22 12:12:59 +0700

    Update Test_3.js

--- Test_3.js
+++ Test_3.js
@@ -1,7 +1,7 @@
 function solution(relation) {
     var answer = 0;
-    for( i = 1 ; i <= relation.length() ; i++){
-        for( j = 1 ; j <= relation.length() ; j++){
+    for( i = 1 ; i <= relation.length; i++){
+        for( j = 1 ; j <= relation.length; j++){
             if(relation[i,1] === relation[j,1]){
                 answer+1;
             }
